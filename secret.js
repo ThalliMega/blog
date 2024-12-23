e3de7afab57ed9f5ddc8909b997ba31148c581de
Thallium Megalovania <ferris@thallimega.win>
2024-12-24 00:05:55 +0800

    ???
    Signed-off-by: Thallium Megalovania <ferris@thallimega.win>

--- secret.js
+++ secret.js
@@ -30,7 +30,7 @@ function happen() {
     p.style = 'text-align: center;font-size: 2em;max-width: 50vw;word-break: break-all;'
     p.textContent = '5LiW55WM5bey5oKE54S25rqc6LWw44CCCuWcqOaIkeeBtemtgua3seWkhArmraTml7bmraTliLvllK/kuIDnmoTnnJ/lrp7kuYvlnLAK5raM6LW35by654OI6ICM5peg5b2i55qE5oKy5Lyk77yMCuS7v+S9m+acieS6uuato+WcqOm7keaal+eahOaIv+mXtOmHjOeXm+WTreOAgg=='
     const audio = document.createElement('audio')
-    audio.src = 'https://files.thallimega.win/129. You Cannot Go Back.mp3'
+    audio.src = 'https://files.thallimega.win/Abnormality%20Third%20Warning%20_Updated%20(LOR%20ver.).mp3'
     audio.autoplay = true
     audio.onended = () => location.reload()
     body.insertAdjacentElement('afterbegin', audio)
